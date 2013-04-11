acfa4139aa43ca6585255e1a07ba898ef4884b3a
Nanda Yadav <nandayadav@gmail.com>
2013-04-10 23:03:08 -0600

    modified tests for visualization

--- lib/rega/visualization.rb
+++ lib/rega/visualization.rb
@@ -4,16 +4,20 @@ module Rega
   class Visualization
     
     #Ignoring viewport
-    attr_accessor :name, :width, :height, :padding
+    attr_reader :name, :width, :height, :padding
     
-    def initialize(name: "", width: 500, height: 500)
+    def initialize(name: "", width: 500, height: 500, **options)
       @name = name
       @width, @height = width, height
+      options.each { |name, value| instance_variable_set("@#{name}", value) }
     end
     
     def attributes
-      {:name => @name, :width => @width, :height => @height}
+      h = {:name => @name, :width => @width, :height => @height}
+      h[:padding] = @padding if @padding
+      h
     end
+    
     
   end
 end
--- test/visualization_test.rb
+++ test/visualization_test.rb
@@ -3,18 +3,12 @@ require_relative 'test_helper'
 class VisualizationTest <  MiniTest::Unit::TestCase
 
   def setup
-    @visualization = Rega::Visualization.new(name: "Bar", height: 200)
+    @visualization = Rega::Visualization.new(name: "Bar", height: 200, padding: { left: 10, right: 10, top: 10 })
   end
       
   def test_width_and_height
     assert_equal @visualization.width, 500, "Width did not match."
     assert_equal @visualization.height, 200, "Height did not match."
-  end
-  
-  def test_attribute_types
-    assert_kind_of String, @visualization.name
-    assert_kind_of Integer, @visualization.width
-    assert_kind_of Integer, @visualization.height
   end
   
   def test_attributes
@@ -23,6 +17,7 @@ class VisualizationTest <  MiniTest::Unit::TestCase
     assert_equal attrs[:height], 200
     assert_equal attrs[:name], "Bar"
     assert_equal attrs[:width], 500
+    assert_equal attrs[:padding][:left], 10
   end
   
 end
